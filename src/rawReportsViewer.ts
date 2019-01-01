946b75b5b14cdddfb51f06d5c207103e9829c9a8
hinst <hinstance@yandex.ru>
2019-01-01 18:20:11 +0100

    refactoring: should create a component for displaying one row of raw report viewer

--- src/rawReportsViewer.ts
+++ src/rawReportsViewer.ts
@@ -1,10 +1,16 @@
 import { durationToText } from './stats'
 import { uiMixin } from './uiMixin';
+import './toggleButton'
 const Vue = require('vue/dist/vue.common.js')
 const fs = require('fs')
 const component = {
     template: fs.readFileSync('./src/rawReportViewer.html').toString(),
     props: ['stats'],
+    data: function() {
+        return {
+            projectsVisible: false,
+        }
+    },
     computed: {
         totalDuration: function() {
             return durationToText(this.stats.totalDuration);
